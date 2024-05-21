9d3d56731c2e8ef60295d00cbc121ad8180592c4
Amene <layla_1380@yahoo.com>
2024-05-21 15:37:23 -0500

    map title case

--- index.js
+++ index.js
@@ -9,8 +9,45 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
+
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+
+  return tutorials.map(sen => {
+    return sen.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ')
+  })
 }
+
+
+
+
+
+
+// function titleCased() {
+//   //const specialWords = { oo: 'OO', api: 'API', nan: 'NaN', stoppropagation:'stoppropagation', preventdefault: 'PreventDefault', jsonp: 'JSONP' }
+//   const specialWords = ['OO', 'API']
+//   return tutorials.map(sen => {
+//     //console.log(sen)
+//     return sen.split(' ').map(word => {
+//       //console.log(word)
+//       //let lowerCaseWords = word.toLowerCase()
+//       if (specialWords.hasOwnProperty(lowerCaseWords)) {
+//         return specialWords[lowerCaseWords]
+//       } else {
+//         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+
+//       }
+//     }).join(' ')
+//   })
+
+// }
+
+
+
+
+console.log(titleCased(tutorials))
+
